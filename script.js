7677ac5740e01b5965d458d8ae261e394aaf1174
Stephen Heinen <stephenheinen22@yahoo.com>
2022-06-15 17:56:40 -0500

    added button functionality, trying to form if/else statement

---
+++ script.js
@@ -0,0 +1,39 @@
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+function promptMe () {
+  var q1 = prompt("Please enter your desired password length (Must be between 8-128).", "answer here");
+  // if (q1 >= 8 && q1 <=128 ) {
+  //  q2()
+  // } else {
+  //   alert("Please choose a number between 8 and 128")
+  //   promptMe();
+  }
+}
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
