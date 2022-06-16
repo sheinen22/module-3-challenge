8beab85e4ca50c53eeee60cd85575fd2a104892a
Stephen Heinen <stephenheinen22@yahoo.com>
2022-06-15 20:56:18 -0500

    added for loop with variables, need to figure out return and how to guarentee that q1 returns a number

--- script.js
+++ script.js
@@ -17,48 +17,65 @@ var charSet = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234
 
 function promptMe () {
   var q1 = prompt("Please enter your desired password length (Must be between 8-128).", "answer here");
-  if (q1 < 8 && q1 > 128 ) {
-   alert("Please choose a number between 8 and 128");
-   promptMe();
-  }
-  var q2 = confirm("Do you want your password to include lowercase letters? Click 'okay' if you want lowercase letters.");
-  var q3 = confirm("Do you want your password to include uppercase letters? Click 'okay' if you want uppercase letters.");
-  var q4 = confirm("Do you want your password to include numbers? Click 'okay' if you want numbers.");
-  var q5 = confirm("Do you want your password to include special characters? Click 'okay' if you want special characters.");
-  var Chars = createCharList(q2, q3, q4, q5)
-  }
-
-  function createCharList (lower, upper, numbers, special) {
-    var charList = ""
-    if (lower){
-      charList += charSet[0];
+  // console.log(q1)
+  if (q1 < 8 || q1 > 128) {
+    alert("Please enter a number between 8 and 128");
+    promptMe();
+  } else {
+      var q2 = confirm("Do you want your password to include lowercase letters? Click 'okay' if you want lowercase letters.");
+      var q3 = confirm("Do you want your password to include uppercase letters? Click 'okay' if you want uppercase letters.");
+      var q4 = confirm("Do you want your password to include numbers? Click 'okay' if you want numbers.");
+      var q5 = confirm("Do you want your password to include special characters? Click 'okay' if you want special characters.");
+      var Chars = createCharList(q2, q3, q4, q5)
+      generatePassword(q1, Chars)
     }
-    if (upper){
-      charList += charSet[1];
-    }
-    if (numbers){
-      charList += charSet[2];
-    }
-    if (special){
-      charList += charSet[3];
-    }
-    if (charList.length === 0) {
-      alert("No characters were chosen")
-    }
-    return charList;
-  }
-  
-
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function createCharList (lower, upper, numbers, special) {
+  var charList = ""
+  if (lower){
+    charList += charSet[0];
+  }
+  if (upper){
+    charList += charSet[1];
+  }
+  if (numbers){
+    charList += charSet[2];
+  }
+  if (special){
+    charList += charSet[3];
+  }
+  if (charList.length === 0) {
+    alert("No characters were chosen")
+  }
+  return charList;
+}
+function generatePassword (passLength, charList) {
+  var password = ""
+  for (let i = 0; i < passLength; i++) {
+    var randomNumber = Math.floor(Math.random() * charList.length)
+    var char = charList[randomNumber]
+    password += char
+    //get the length of the charlist
+    //generate a random character for every place in the length ie if length = 8, pull 8 random characters from charlist
+    //select from charlist the index position of the random number we created
+    //add selected character to password variable
+  }
+  // return password
+  console.log(password)
+}
+
+
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
